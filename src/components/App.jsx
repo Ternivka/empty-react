0316f6f9f57d6c896441f70fee6388c384839483
yeah <154704827+Ternivka@users.noreply.github.com>
2024-12-23 14:27:33 +0100

    add some components

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import Product from "./Product";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
